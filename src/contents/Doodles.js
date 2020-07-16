89358a4092d9c3b807f10f0b23748cf0a607f359
Mimi Shalf <katharine.shalf@berkeley.edu>
2020-07-15 19:51:35 -0700

    debugging

--- src/contents/Doodles.js
+++ src/contents/Doodles.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 import Gallery from 'react-grid-gallery';
 
+import hug1 from '../img/illustrations/anotherhug.png';
 import cat from '../img/illustrations/cat.png';
 import chair from '../img/illustrations/chair.png';
 import devan from '../img/illustrations/Devan.png';
@@ -50,136 +51,135 @@ class Doodles extends Component {
                     minHeight: "1px",
                     width: "100%",
                     overflow: "auto"}}>
-	             <Gallery
-		            images={this.state.images}
-		            enableLightbox={true}
-		            enableImageSelection={false}
-		            margin={0}
-		            rowHeight={290}/>
+	             
              </div>
 	    );
   	}
 }
 
-Doodles.propTypes = {
-    images: PropTypes.arrayOf(
-        PropTypes.shape({
-            src: PropTypes.string.isRequired,
-            thumbnail: PropTypes.string.isRequired,
-            srcset: PropTypes.array,
-            caption: PropTypes.oneOfType([
-                PropTypes.string,
-                PropTypes.element
-            ]),
-            thumbnailWidth: PropTypes.number.isRequired,
-            thumbnailHeight: PropTypes.number.isRequired
-        })
-    ).isRequired
-};
+// Doodles.propTypes = {
+//     images: PropTypes.arrayOf(
+//         PropTypes.shape({
+//             src: PropTypes.string.isRequired,
+//             thumbnail: PropTypes.string.isRequired,
+//             srcset: PropTypes.array,
+//             caption: PropTypes.oneOfType([
+//                 PropTypes.string,
+//                 PropTypes.element
+//             ]),
+//             thumbnailWidth: PropTypes.number.isRequired,
+//             thumbnailHeight: PropTypes.number.isRequired
+//         })
+//     ).isRequired
+// };
 
-Doodles.defaultProps = {
-    images: [
-        {
-            src: cat,
-            thumbnail: cat
-        },
-        {
-            src: chair,
-            thumbnail: chair
-        },
-        {
-            src: devan,
-            thumbnail: devan
-        },
-        {
-            src: dogview,
-            thumbnail: dogview
-        },
-        {
-            src: elise,
-            thumbnail: elise
-        },
-        {
-            src: face,
-            thumbnail: face
-        },
-        {
-            src: florist,
-            thumbnail: florist,
-        },
-        {
-            src: fruit,
-            thumbnail: fruit,
-        },
-        {
-            src: hometown,
-            thumbnail: hometown,
-        },
-        {
-            src: jolana,
-            thumbnail: jolana,
-        },
-        {
-            src: hug,
-            thumbnail: hug,
-        },
-        {
-            src: indianrock,
-            thumbnail: indianrock,
-        },
-        {
-            src: kiki1,
-            thumbnail: kiki1,
-        },
-        {
-            src: kiss,
-            thumbnail: kiss,
-        },
-        {
-            src: lai,
-            thumbnail: lai,
-        },
-        {
-            src: matthewfuture,
-            thumbnail: matthewfuture,
-        },
-        {
-            src: meetme,
-            thumbnail: meetme,
-        },
-        {
-            src: mess,
-            thumbnail: mess,
-        },
-        {
-            src: serena,
-            thumbnail: serena,
-        },
-        {
-            src: family,
-            thumbnail: family,
-        },
-        {
-            src: mom,
-            thumbnail: mom,
-        },
-        {
-            src: mind,
-            thumbnail: mind,
-        },
-        {
-            src: nick,
-            thumbnail: nick,
-        },
-        {
-            src: princess,
-            thumbnail: princess,
-        },
-        {
-            src: sleep,
-            thumbnail: sleep,
-        },
-    ]
-};
+// Doodles.defaultProps = {
+//     images: [
+//         {
+//             src: hug1,
+//             thumbnail: hug1,
+//         },
+//         {
+//             src: cat,
+//             thumbnail: cat
+//         },
+//         {
+//             src: chair,
+//             thumbnail: chair
+//         },
+//         {
+//             src: devan,
+//             thumbnail: devan
+//         },
+//         {
+//             src: dogview,
+//             thumbnail: dogview
+//         },
+//         {
+//             src: elise,
+//             thumbnail: elise
+//         },
+//         {
+//             src: face,
+//             thumbnail: face
+//         },
+//         {
+//             src: florist,
+//             thumbnail: florist,
+//         },
+//         {
+//             src: fruit,
+//             thumbnail: fruit,
+//         },
+//         {
+//             src: hometown,
+//             thumbnail: hometown,
+//         },
+//         {
+//             src: jolana,
+//             thumbnail: jolana,
+//         },
+//         {
+//             src: hug,
+//             thumbnail: hug,
+//         },
+//         {
+//             src: indianrock,
+//             thumbnail: indianrock,
+//         },
+//         {
+//             src: kiki1,
+//             thumbnail: kiki1,
+//         },
+//         {
+//             src: kiss,
+//             thumbnail: kiss,
+//         },
+//         {
+//             src: lai,
+//             thumbnail: lai,
+//         },
+//         {
+//             src: matthewfuture,
+//             thumbnail: matthewfuture,
+//         },
+//         {
+//             src: meetme,
+//             thumbnail: meetme,
+//         },
+//         {
+//             src: mess,
+//             thumbnail: mess,
+//         },
+//         {
+//             src: serena,
+//             thumbnail: serena,
+//         },
+//         {
+//             src: family,
+//             thumbnail: family,
+//         },
+//         {
+//             src: mom,
+//             thumbnail: mom,
+//         },
+//         {
+//             src: mind,
+//             thumbnail: mind,
+//         },
+//         {
+//             src: nick,
+//             thumbnail: nick,
+//         },
+//         {
+//             src: princess,
+//             thumbnail: princess,
+//         },
+//         {
+//             src: sleep,
+//             thumbnail: sleep,
+//         },
+//     ]
+// };
  
 export default Doodles;
