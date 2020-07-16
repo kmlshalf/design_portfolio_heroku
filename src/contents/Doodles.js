58004c7f4fcf7916dfef0247467963e9b8a1131c
Mimi Shalf <katharine.shalf@berkeley.edu>
2020-07-15 19:54:26 -0700

    commented out buggy code

--- src/contents/Doodles.js
+++ src/contents/Doodles.js
@@ -4,36 +4,36 @@ import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 import Gallery from 'react-grid-gallery';
 
-import hug1 from '../img/illustrations/anotherhug.png';
-import cat from '../img/illustrations/cat.png';
-import chair from '../img/illustrations/chair.png';
-import devan from '../img/illustrations/Devan.png';
-import dogview from '../img/illustrations/dogview.png';
-import elise from '../img/illustrations/Elise.png';
-import face from '../img/illustrations/face.png';
-import family from '../img/illustrations/Family.png';
-import florist from '../img/illustrations/Florist.png';
-import fruit from '../img/illustrations/Fruit.png';
-import hometown from '../img/illustrations/Hometown.png';
-import indianrock from '../img/illustrations/Indian_Rock.png';
-import jolana from '../img/illustrations/jolanapresent.jpg';
-import kiki1 from '../img/illustrations/Kiki 1.png';
-import kiki from '../img/illustrations/Kiki.png';
-import kiss from '../img/illustrations/kiss.png';
-import lai from '../img/illustrations/Lai.png';
-import matthewfuture from "../img/illustrations/Matthew's_Future.png";
-import hug from '../img/illustrations/hug.png';
-import meetme from '../img/illustrations/Meet_The_Designer.png';
-import mess from '../img/illustrations/Mess.png';
-import mom from '../img/illustrations/Mom.png';
-import mind from '../img/illustrations/My_Mind_During_Work_.png';
-import nick from '../img/illustrations/Nick_Present.png';
-import princess from '../img/illustrations/Princess.png';
-import ronald from '../img/illustrations/ronaldmcdonald.png';
-import sailormoon from '../img/illustrations/sailormoon.png';
-import serena from '../img/illustrations/Serena_Bday.png';
-import sleep from '../img/illustrations/Sleep.png';
-import sleeping from '../img/illustrations/sleeping.png';
+// import hug1 from '../img/illustrations/anotherhug.png';
+// import cat from '../img/illustrations/cat.png';
+// import chair from '../img/illustrations/chair.png';
+// import devan from '../img/illustrations/Devan.png';
+// import dogview from '../img/illustrations/dogview.png';
+// import elise from '../img/illustrations/Elise.png';
+// import face from '../img/illustrations/face.png';
+// import family from '../img/illustrations/Family.png';
+// import florist from '../img/illustrations/Florist.png';
+// import fruit from '../img/illustrations/Fruit.png';
+// import hometown from '../img/illustrations/Hometown.png';
+// import indianrock from '../img/illustrations/Indian_Rock.png';
+// import jolana from '../img/illustrations/jolanapresent.jpg';
+// import kiki1 from '../img/illustrations/Kiki 1.png';
+// import kiki from '../img/illustrations/Kiki.png';
+// import kiss from '../img/illustrations/kiss.png';
+// import lai from '../img/illustrations/Lai.png';
+// import matthewfuture from "../img/illustrations/Matthew's_Future.png";
+// import hug from '../img/illustrations/hug.png';
+// import meetme from '../img/illustrations/Meet_The_Designer.png';
+// import mess from '../img/illustrations/Mess.png';
+// import mom from '../img/illustrations/Mom.png';
+// import mind from '../img/illustrations/My_Mind_During_Work_.png';
+// import nick from '../img/illustrations/Nick_Present.png';
+// import princess from '../img/illustrations/Princess.png';
+// import ronald from '../img/illustrations/ronaldmcdonald.png';
+// import sailormoon from '../img/illustrations/sailormoon.png';
+// import serena from '../img/illustrations/Serena_Bday.png';
+// import sleep from '../img/illustrations/Sleep.png';
+// import sleeping from '../img/illustrations/sleeping.png';
 
 class Doodles extends Component {
 	 constructor(props){
@@ -51,7 +51,12 @@ class Doodles extends Component {
                     minHeight: "1px",
                     width: "100%",
                     overflow: "auto"}}>
-	             
+	             // <Gallery
+		            // images={this.state.images}
+		            // enableLightbox={true}
+		            // enableImageSelection={false}
+		            // margin={0}
+		            // rowHeight={290}/>
              </div>
 	    );
   	}
